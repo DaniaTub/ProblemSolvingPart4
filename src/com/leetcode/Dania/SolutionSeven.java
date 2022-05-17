f504875792068e0bc30b5ed8e373b8652d78331a
Dania <daniatub8@gmail.com>
2022-05-17 20:18:03 +0300

    Merge two linkedList

--- src/com/leetcode/Dania/SolutionSeven.java
+++ src/com/leetcode/Dania/SolutionSeven.java
@@ -1,5 +1,33 @@
 package com.leetcode.Dania;
 //Given head, the head of a linked list, determine if the linked list has a cycle in it.
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class SolutionSeven
 {
+    public boolean hasCycle(ListNode head)
+    {
+        ListNode fast=head,slow=head;
+        if(head==null) return false;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+
 }
